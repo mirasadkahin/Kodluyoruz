55cad053b4735ed6b00b05c28f84c242bea48036
Eray-C <ciftcieray585@gmail.com>
2025-02-01 15:11:42 +0300

    End of Chapter Exercise commit

---
+++ Javascript-egitimi/js/019-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyarız-ve-ilk-fonksiyonumuz.js
@@ -0,0 +1,27 @@
+// ** Ilk Fonksiyonumuzu Tanimlamak:
+
+
+function helloWord (){
+    console.log("Hello World")
+}
+function hello() {
+    console.log("Merhaba")
+    helloWord()
+}
+
+
+// hata alabiliriz..
+// function userCheck() {
+//    if (userName && age >= 18 ) {
+//        info.innerHTML = "Ehliyet alabilirsiniz"
+//    }
+//    else if (!userName) {
+//        info.innerHTML = "Kullanızı Adı Yok"
+//    }
+//    else if (!(age>=18)){
+//        info.innerHTML = "Yaş Bilginiz Yok veya 18 yaşından küçüksünüz"
+//    }
+// }
+
+
+hello() // calistir
